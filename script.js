2fb5c9a66ef70ee761b0141e711711cdf8429b72
Plab0m <nseipablo@gmail.com>
2025-08-01 23:31:01 -0300

    Progresso javascript

--- script.js
+++ script.js
@@ -1,15 +1,51 @@
+const valorinput = document.getElementsByClassName("input-valor")[0];
+const SelectFrom = document.querySelector("#select-moeda1");
+const SelectTo = document.querySelector("#select-moeda2");
+const ImgResult1 = document.querySelector(".resultado1-logo");
+const ImgResult2 = document.querySelector(".resultado2-logo");
+const ConvertFromValue = document.querySelector("#valueResult1");
+const ConvertedValue = document.querySelector("#valueResult2");
+const MoedaResult1 = document.querySelector("#moedaResult1");
+const MoedaResult2 = document.querySelector("#moedaResult2");
+
 function Converter() {
-    const valorinput = document.getElementsByClassName("input-valor")[0];
-    console.log(valorinput.value);
-    const dolarTday = 5.54;
-    const euroTday = 6.05;
-    const realTday = 1.00;
+  ConvertFromValue.innerHTML = new Intl.NumberFormat("pt-BR", {
+    style: "currency",
+    currency: "BRL",
+  }).format(valorinput.value);
 
-    const ConvertFromValue = document.querySelector("#valueResult1");
-    ConvertFromValue.innerHTML = "R$ " + Number(valorinput.value).toFixed(2);
+  const dolar = 5.54;
+  const euro = 6.05;
+  const real = 1.0;
 
-    const ConvertedValue = document.querySelector("#valueResult2");
-    ConvertedValue.innerHTML = "US$ "+ (valorinput.value * dolarTday).toFixed(2);
+  if (SelectTo.value == "Dolar") {
+    ConvertedValue.innerHTML = new Intl.NumberFormat("en-US", {
+      style: "currency",
+      currency: "USD",
+    }).format(valorinput.value * dolar);
+  }
+  if (SelectTo.value == "Euro") {
+    ConvertedValue.innerHTML = new Intl.NumberFormat("de-DE", {
+      style: "currency",
+      currency: "EUR",
+    }).format(valorinput.value * euro);
+  }
 }
 
+SelectTo.addEventListener("change", ChangeMoeda);
+function ChangeMoeda() {
+  Converter();
+  MoedaResult2.innerHTML = SelectTo.value;
+  const bandeiras = {
+    Dolar: "assets/usaBandeira.png",
+    Real: "assets/brasilBandeira.png",
+    Euro: "assets/EURBandeira.png",
+  };
 
+  if (bandeiras[SelectFrom.value]) {
+    ImgResult1.src = bandeiras[SelectFrom.value];
+  }
+  if (bandeiras[SelectTo.value]) {
+    ImgResult2.src = bandeiras[SelectTo.value];
+  }
+}
