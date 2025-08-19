83b54f1e506be17ba5b33bdcf973ef1c45926361
Plab0m <nseipablo@gmail.com>
2025-08-18 22:16:43 -0300

    alterando a lógica para usar uma API em vez de valores fixos.

--- script.js
+++ script.js
@@ -8,83 +8,72 @@ const ConvertedValue = document.querySelector("#valueResult2");
 const MoedaResult1 = document.querySelector("#moedaResult1");
 const MoedaResult2 = document.querySelector("#moedaResult2");
 
-const moedas = {
-  Dolar: 5.54,
-  Euro: 6.42,
-  Real: 1.0,
-};
-
 const formatacaoMoedas = {
-  Dolar: { locale: "en-US", currency: "USD" },
-  Euro: { locale: "de-DE", currency: "EUR" },
-  Real: { locale: "pt-BR", currency: "BRL" },
+  USDBRL: { locale: "en-US", currency: "USD" },
+  EURBRL: { locale: "de-DE", currency: "EUR" },
+  BRL: { locale: "pt-BR", currency: "BRL" },
 };
 
-function converterParaReal(valor, moedaOrigem) {
-  return valor * moedas[moedaOrigem];
-}
-
-function converterDeReal(valorEmReal, moedaDestino) {
-  return valorEmReal / moedas[moedaDestino];
-}
-
-function converterMoedas(valor, moedaOrigem, moedaDestino) {
+async function converterMoedas(valor, moedaOrigem, moedaDestino) {
+  const moedas = await fetch(
+    "https://economia.awesomeapi.com.br/last/USD-BRL,EUR-BRL,BTC-BRL"
+  ).then((response) => response.json());
   if (moedaOrigem === moedaDestino) {
     return valor;
   }
-
-  const valorEmReal = converterParaReal(valor, moedaOrigem);
-
-  const valorConvertido = converterDeReal(valorEmReal, moedaDestino);
-
+  const cotOrigem = moedaOrigem == "BRL" ? 1 : parseFloat(moedas[moedaOrigem].high);
+  const cotDestino = moedaDestino == "BRL" ? 1 : parseFloat(moedas[moedaDestino].high);
+  const valorConvertido = (valor * cotOrigem) / cotDestino;
   return valorConvertido;
 }
 
-function Converter() {
+async function Converter() {
   const valorOriginal = parseFloat(valorinput.value) || 0;
   const moedaOrigem = SelectFrom.value;
   const moedaDestino = SelectTo.value;
+
+  const valorConvertido = await converterMoedas(
+    valorOriginal,
+    moedaOrigem,
+    moedaDestino
+  );
 
   const formatOrigem = formatacaoMoedas[moedaOrigem];
   ConvertFromValue.innerHTML = new Intl.NumberFormat(formatOrigem.locale, {
     style: "currency",
     currency: formatOrigem.currency,
   }).format(valorOriginal);
-
-  const valorConvertido = converterMoedas(
-    valorOriginal,
-    moedaOrigem,
-    moedaDestino
-  );
+  const formatDestino = formatacaoMoedas[moedaDestino];
+  ConvertedValue.innerHTML = new Intl.NumberFormat(formatDestino.locale, {
+    style: "currency",
+    currency: formatDestino.currency,
+  }).format(valorConvertido);
 
   if (valorOriginal <= 0) {
     valorinput.placeholder = "Valor inválido!";
     valorinput.classList.add("erro-placeholder");
     return;
   }
-  const formatDestino = formatacaoMoedas[moedaDestino];
-  ConvertedValue.innerHTML = new Intl.NumberFormat(formatDestino.locale, {
-    style: "currency",
-    currency: formatDestino.currency,
-  }).format(valorConvertido);
+
   valorinput.classList.remove("erro-placeholder");
   valorinput.placeholder = "Insira o valor:";
 }
 const ButtonConverter = document.querySelector(".converter");
-ButtonConverter.addEventListener("click", Converter);
 ButtonConverter.addEventListener("click", ChangeMoeda);
 SelectTo.addEventListener("change", ChangeMoeda);
 SelectFrom.addEventListener("change", ChangeMoeda);
 
 function ChangeMoeda() {
   Converter();
-  MoedaResult1.innerHTML = SelectFrom.value;
-  MoedaResult2.innerHTML = SelectTo.value;
+  MoedaResult1.innerHTML =
+    SelectFrom.value == "BRL" ? "Real" : SelectFrom.value.slice(0, 3);
+  MoedaResult2.innerHTML =
+    SelectTo.value == "BRL" ? "Real" : SelectTo.value.slice(0, 3);
 
   const bandeiras = {
-    Dolar: "assets/usaBandeira.png",
-    Real: "assets/brasilBandeira.png",
-    Euro: "assets/EURBandeira.png",
+    USDBRL: "assets/usaBandeira.png",
+    BRL: "assets/brasilBandeira.png",
+    EURBRL: "assets/EURBandeira.png",
   };
 
   if (bandeiras[SelectFrom.value]) {
